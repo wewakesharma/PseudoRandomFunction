edfc17a75d9e0057bf756559f2a35b1af3740965
Tzipora Halevi <thalevi@nyu.edu>
2020-03-11 10:31:40 -0400

    updated pi_protocol

--- pi_protocol.cpp
+++ pi_protocol.cpp
@@ -257,17 +257,19 @@ int main()
     //cin>>choice;
     chrono::time_point<std::chrono::system_clock> start, end; 
     start = chrono::system_clock::now(); 
-    for(int i=0;i<1000;i++){
+    for(int i=0;i<10;i++){
     generateInput();
-    setshare();
-    /*if(choice == 'y' || choice == 'Y')
-    {
-        display_values();
-    }*/
-    S();
-    pi_23();
-    verify_pi();
-    cout<<endl;
+        for(int i=0;i<100;i++) {
+            setshare();
+            /*if(choice == 'y' || choice == 'Y')
+            {
+                display_values();
+            }*/
+            S();
+            pi_23();
+        }
+   // verify_pi();
+    //cout<<endl;
 }
     end = chrono::system_clock::now();
     chrono::duration<double> elapsed_seconds = end - start; 
