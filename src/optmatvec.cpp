dc89af6714169a4e3588df2223821181ad49b772
vivek18394 <vivek18394@gmail.com>
2020-04-26 14:27:19 -0400

    working for one iteration

--- src/optmatvec.cpp
+++ src/optmatvec.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 #include <bitset>
 #include <cmath>
 #include <chrono> 
@@ -9,7 +10,7 @@
 int len=256; //len is the value of security parameter, also the dimension of matrix
 int row, col;
 int secret_key[256][256];
-int input[256];
+
 int toep_values[512];//Contains bits 
 int packed_key[256];
 int temp_input[4];
@@ -21,212 +22,76 @@ unsigned long int packed_int = 0;
 using namespace std;
 
 //This function will create the input vector randomly and store that value into the array named input.
-void generate_input()
+void generate_input(bool input[256], std::mt19937 &generator)
 {
 	srand(time(NULL));
-    for(int i = 0; i < len; i++)
+    for(int i = 0; i < 256; i++)
     {
-        input[i] = rand() & 1;
+        input[i] = generator() & 1;
     }
 }
 
-
-void generate_toeplitz()
+//This function will create the input vector randomly and store that value into the array named input.
+void generate_rand_key(uint64_t key[4][256], std::mt19937 &generator)
 {
-	//generating random number for toeplitz values. Toeplitz will have two parts base and append. base will be decreasing and append ptr
-	//will increase
-	srand(time(NULL));
-	//total number to be generated for a toeplitz matrix is (2*n - 1)
-	for(int i = 0; i < (2*len-1); i++)
-	{
-		toep_values[i] = rand() & 1;
-	}
-
-	int append = len;
-	int base = len;
-	int k = 0;
-	//Filling up the top part of toeplitz matrix
-	
-	for(int i = 0; i < len; i++)
-	{
-		//Filling up the upper triangular matrix
-		int k = 0;
-		for(int j = i; j < len;j++)
-		{
-			secret_key[i][j] = toep_values[k];
-			k++;
-		}
-
-		for(int j = 0; j < i+1; j++)
-		{
-			secret_key[i+1][j] = toep_values[(base-j)];
-		}
-		base++;
-	}	
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 256; j++)
+        {
+            key[i][j] = generator();
+        }
+    }
 }
 
-void display_values()
+void compute(uint64_t key[4][256], bool input[256])
 {
-	cout<<"The input generated is "<<std::endl;
-	for(int i = 0; i < len; i++)
-	{
-		std::cout<<input[i];
-	}
-	std::cout<<endl;
-	cout<<"The toeplitz key generated is "<<std::endl;
-	for(int i = 0; i < (2*len-1); i++)
-	{
-		std::cout<<toep_values[i];
-	}
-	std::cout<<endl;
-	cout<<"The toeplitz secret key matrix generated is "<<std::endl;
-	for(int i = 0; i < len; i++)
-	{
-		for(int j = 0; j < len; j++)
-		{
-			std::cout<<secret_key[i][j]<<"\t";
-		}
-		std::cout<<"\n";
-	}
-	std::cout<<endl;
-}
-
-//This code needs to be removed. Waiting for final confirmation
-/*void calculate()
-{
-	//int temp_input[4];
-	int temp_key[4];
-
-	//Calculating Z1.
-
-
-	//Calculating Z1
-	for(int col = 0; col < len/2; col++)
-	{
-		if(input[(len/2) - col - 1] == 1)
-		{
-			for(int row = 0; row < len/2; row++)
-			{
-				z1[row] = z1[row] + secret_key[row][col];
-			}
-		}
-	}
-	
-	//Calculating Z2
-	for(int col = len/2; col < len; col++)
-	{
-		if(input[(len) - col - 1] == 1)
-		{
-			for(int row = len/2; row < len; row++)
-			{
-				z1[row] = z1[row] + secret_key[row][col];
-			}
-		}
-	}
-
-	//Displaying the values of temp key
-	for(int i = 0; i < len/2; i++)
-	{
-		cout<<temp_key[i];
-	}
-}
-
-void compute()
-{
-	//int pack_size = 4;
-	int temp;
-	
-
-}*/
-
-void pack()//packing the bit in secret key.
-{	
-	int i = 0;
-	//long temp = 0;
-	int k;
-	for(int col = 0; col < len; col++)//for each column
-	{
-		for(int row = 0;row < len; row=row+pack_size)//a pack of 64 bit as a single word
-		{
-			k = 0;
-			packed_int = 0;
-			for(int j = pack_size-1; j >= 0; j--)
-			{
-				packed_int = packed_int + secret_key[row+k][col]*pow(2,j);
-				k++;
-			}
-			//cout<<packed_int<<endl;
-			//int temp = secret_key[row][col]*pow(2,3) + secret_key[row+1][col]*pow(2,2) + secret_key[row+2][col]*pow(2,1) + secret_key[row+3][col];
-			packed_key[i] = packed_int;
-			i++;
-		}
-		row = 0;
-	}
-
-	//The following code needs to be removed. Waiting for final confirmation
-	//cout<<"Value of i "<<i<<endl;
-	/*for(int i=0;i<2*len;i++)
-	{
-		cout<<packed_key[i]<<endl;
-	}*/
-}
-
-/*void compute()
-{
-	int z1 = 0;
-	int z2 = 0;
-	int temp;
-	int d;
-	//compute z1
-	for(int i = 0; i < len/pack_size; i++)
-	{
-		for(int j = 0; j < pack_size; j++)
-		{
-			d = 4*i;
-			z1 = ((input[d+j] * packed_key[8*i + 6]) ^ 
-			(input[d+j] * packed_key[8*i + 4]) ^
-				(input[d+j] * packed_key[8*i + 2]) ^
-					(input[d+j] * packed_key[8*i]));
-		}
-	}
-	cout<<"Z1 is "<<z1<<endl;
-
-	//compute z2
-	for(int i = 0; i < len/pack_size; i++)
-	{
-		for(int j = 0; j < pack_size; j++)
-		{
-			d = 4*i;
-			z2 = ((input[d+j] * packed_key[8*i + 7]) ^ 
-			(input[d+j] * packed_key[8*i + 5]) ^
-				(input[d+j] * packed_key[8*i + 3]) ^
-					(input[d+j] * packed_key[8*i + 1]));
-		}
-	}
-	cout<<"Z2 is "<<z2<<endl;
-}*/
-
-void compute()
-{
-	for(int i=0; i < len/pack_size; i++)
+	for(int i=0; i < 4; i++)
 	{
 		z_final[i] = 0;
-		for(int j = 0; j < len; j++)
+		for(int j = 0; j < 256; j++)
 		{
-			z_final[i] = z_final[i] ^ (input[j] * packed_key[4*j+i]);
+			if(input[j]==1)
+			{
+				z_final[i] = z_final[i] ^ (key[i][j]);
+			}	
 		}
-		//cout<<"Value of z"<<i<<" is "<<z_final[i]<<endl;
+		cout<<endl<<"Value of z"<<i<<" is "<<z_final[i];
 	}
 }
 
 //Step 1: Generate random input and store it in input array.
-//Step 2: Generate 2n-1 bits and store it toep_values array. 
-//Step 3: Create a nxn toeplitz matrix. This step can be bypassed since we no longer need the bits in matrix but packed in a word.
-//Step 4: pack() function will pack four bits into one.
-//Step 5: compute() function will calculate the value of z1 and z2.
+//Step 2: Generate 1024 uint64 numbers and store it in 4X256 size.
+//Step 3: compute() function will calculate the value of zi's
 int main()
 {
-	chrono::time_point<std::chrono::system_clock> start, end; 
+	uint64_t key[4][256];
+	bool input[256];
+	unsigned seed = 7;            // std::chrono::system_clock::now().time_since_epoch().count();
+
+    std::mt19937 generator(seed); // mt19937 is a standard mersenne_twister_engine
+	generate_rand_key(key, generator);
+
+	//Key value display
+	std::cout<<"Displaying the value of keys";
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 256; j++)
+        {
+            std::cout<<key[i][j];
+            std::cout<<"\t";
+        }
+        std::cout<<"\n\n";
+    }
+
+    //input value display
+    std::cout<<"displaying the value of input";
+    generate_input(input,generator);
+    for(int i=0;i<len;i++)
+    {
+    	cout<<input[i];
+    }
+    compute(key,input);
+	/*chrono::time_point<std::chrono::system_clock> start, end; 
     start = chrono::system_clock::now(); 
     for(int i=0;i<10;i++)
     {
@@ -245,5 +110,6 @@ int main()
   
     cout << "Finished at " << ctime(&end_time) 
               << "elapsed time: " << elapsed_seconds.count() << "s\n";
+              */
 	return 0;
 }
