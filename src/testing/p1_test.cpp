380a1c912074ff752eaf3a333814a0281022a925
Tzipora Halevi <thalevi@nyu.edu>
2020-06-03 15:10:13 -0400

    updated p1_test

--- src/testing/p1_test.cpp
+++ src/testing/p1_test.cpp
@@ -26,6 +26,15 @@ void generate_input(uint64_t input[4], std::mt19937 &generator)
     }
 }
 
+void generate_debug_input(uint64_t input[4])
+{
+    input[0] = 1;
+    for(int i = 1; i < 4; i++)
+    {
+        input[i] = 0; //debug input is only 1 bit per word
+    }
+}
+
 /*
  * Generate the random key matrix, which is 256x256
  */
@@ -40,21 +49,52 @@ void generate_rand_key(uint64_t key[4][256], std::mt19937 &generator)
     }
 }
 
-//Testing code for phase 1 matrix vector multiplication using naive implementation.
-void mat_vec_mult(uint64_t input[4], uint64_t key[4][256], bool out[256])
+void generate_debug_key(uint64_t key[4][256])
 {
-    int column_total = 0;
-	for(int col_count = 0; col_count < 256; col_count++) 		//we have to traverse 256 columns in key matrix
+    for (int i = 0; i < 4; i++)
     {
-        column_total = 0;
-        for(int row_count = 0; row_count < 4; row_count++) 			//traverses through 4 rows of each column in both key and input
+        for (int j = 0; j < 256; j++)
         {
-            for(int word_count = 0; word_count < wLen; word_count++)		//each bit of a word
-            {
-                column_total = column_total + (((input[row_count]>>word_count)&1) * ((key[row_count][col_count]>>word_count)&1));
+            key[i][j] = 255;
+        }
+    }
+}
+
+//Testing code for phase 1 matrix vector multiplication using naive implementation.
+void mat_vec_mult(uint64_t input[4], uint64_t key[4][256], int out[256])
+{
+    //create a key matrix
+    //do bit by bit calculation
+
+    uint64_t keyMatrix[256][256];
+    uint64_t inputVector [256];
+
+    for (int iRow = 0; iRow< 256; iRow++)
+    {
+        uint64_t iWord = floor(iRow/wLen);
+        uint64_t iBit = iRow%wLen;
+        uint64_t inputWord = input[iWord];
+        uint64_t tempVar = inputWord >> (iBit);
+        if (iRow==32)
+            cout << tempVar << "\n";
+        inputVector[iRow] = (inputWord >> (iBit)) & 1;
+    }
+
+
+        for (int iRow = 0; iRow< 256; iRow++) {
+            int iWord = floor(iRow / wLen);
+            for (int iCol = 0; iCol < 256; iCol++) {
+                uint64_t key_word = key[iWord][iCol];
+                keyMatrix[iRow][iCol] = (key_word >> (iRow % wLen)) & 1;
             }
         }
-        out[col_count] = (column_total%2);
+
+    for (int iRow = 0; iRow< 256; iRow++) {
+        int row_total = 0;
+        for (int iCol = 0; iCol < 256; iCol++) {
+            row_total += keyMatrix[iRow][iCol] * inputVector[iCol];
+        }
+        out[iRow] = (row_total%2);
     }
 }
 
@@ -73,19 +113,46 @@ void compute(uint64_t key[4][256], uint64_t input[4], uint64_t z_final[4])//comp
             uint64_t y = -((x>>j) & 1);
 
             for (int k = 0; k < 4; k++) {
-                uint64_t z = key[k][j+wLen*i];//Take each key from the key matrix. row by row
+                uint64_t z = key[k][j+(wLen*i)]; //Take each key from the key matrix. row by row
                 z_final[k] ^= (z & y);//bitwise AND with the y.
             }
 		}
 	}
 }
 
+void compute_debug(uint64_t key[4][256], uint64_t input[4], uint64_t z_final[4])//compute() is equivalent to matvecmultiplication
+{
+    for(int i=0; i < 4; i++) {
+        z_final[i] = 0; // initialize the accumulator to zero
+    }
+
+    int i = 0;
+    //for(int i=0; i < 4; i++)//traverse through the input(4 words)
+    {
+        uint64_t x = input[i]; //takes each word of input
+        int j = 0; //only look at the first big of the each
+     //   for(int j = 0; j < wLen; j++)//traverse through each bit of each input
+        {
+            uint64_t y = -((x>>j) & 1);
+
+            for (int k = 0; k < 4; k++) {
+                uint64_t z = key[k][j+(wLen*i)]; //Take each key from the key matrix. row by row
+                z_final[k] ^= (z & y);//bitwise AND with the y.
+                int temp = z_final[0];
+                int firstbit = temp&1;
+                int firstkeybit = key[0][0] & 1;
+                cout << temp << " first bit" << firstbit << "\n";
+            }
+        }
+    }
+}
+
 /*
 * The function compares the output of unpacked version and output of naive implementation
 */
-void final_test(uint64_t z_final[4], bool out[256])
+void final_test(uint64_t z_final[4], int out[256])
 {
-	bool pack_temp_out[256];
+	int pack_temp_out[256];
 	int cnt=0;
     for(int i = 0; i < 4; i++)
     {
@@ -105,6 +172,8 @@ void final_test(uint64_t z_final[4], bool out[256])
     		p_count++;
     	if(out[i] == 1)
     		out_count++;
+    	if ((pack_temp_out[i] != out[i]))
+    	    cout << "error in bit " <<  i << endl;
     }
     cout<<"P_count is "<<p_count<<endl;
     cout<<"out count is "<<out_count<<endl;	//Printing out the count of set bits of out and pack_temp_out
@@ -130,7 +199,7 @@ int main()
 	uint64_t key[4][256];
     uint64_t input[4];
     int unpacked_input[256];
-    bool out[256];  //variable used for naive testing, represents output of phase 1
+    int out[256];  //variable used for naive testing, represents output of phase 1
     uint64_t output[4];
 
     unsigned seed = 7;    // std::chrono::system_clock::now().time_since_epoch().count();
@@ -139,9 +208,15 @@ int main()
 
 	//we generate two random matrices, one holds the first bit and one the second bit
     generate_input(input,generator);
+
+    //generate_debug_input(input);
+   // generate_debug_key(key);
+
     mat_vec_mult(input, key, out);
     
     compute(key,input, output); // matrix-vector multiply mod 2
+
+    //compute_debug(key,input, output); // matrix-vector multiply mod 2
 
     final_test(output,out);//compares the output of two different approaches
 	return 0;
