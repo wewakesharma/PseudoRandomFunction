51dab8a5cf14e7c149131b2e26e3dbe67eb11f94
vivek18394 <vivek18394@gmail.com>
2020-06-06 11:38:35 -0400

    debug

--- src/testing/unit_test.cpp
+++ src/testing/unit_test.cpp
@@ -68,6 +68,8 @@ void generate_rand_matrix(uint64_t randMat1[2][256], uint64_t randMat2[2][256], 
                     //make sure we don't have 11 - this is a mod 2 matrix so we can only have 00, 01 or 10
                     if (!((bit1 == 1) & (bit2 == 1)))
                     {
+                    	//cout<<bit1<<"\t"<<bit2<<((bit1<<1) | bit2)<<endl;
+
                         //assign the next bits generated to their locations in the random matrices, the location is nBitsFound
                         randMat1[i][j] |= (bit1 << nBitsGenerated);
                         randMat2[i][j] |= (bit2 << nBitsGenerated);
@@ -78,11 +80,13 @@ void generate_rand_matrix(uint64_t randMat1[2][256], uint64_t randMat2[2][256], 
                     }
                 }
             }
+            cout<<endl;
 
         }
     }
     //displaying what is inside randMat1 and randMat2
     int x,y;
+    cout<<"Displaying the contents of randMat1 and randMat2 : after storing";
     for(int i = 0; i< 2; i++)
     {
     	for(int j = 0; j<256; j++)
@@ -91,7 +95,7 @@ void generate_rand_matrix(uint64_t randMat1[2][256], uint64_t randMat2[2][256], 
     		{
     			x = ((randMat1[i][j] >> k) & 1);
     			y = ((randMat2[i][j] >> k) & 1);
-    			cout<<(( x<<1) | y)<<"\t";
+    			cout<<x<<"\t"<<y<<"\t"<<(( x<<1) | y)<<"\t";
     		}
     	}
     	cout<<endl;
