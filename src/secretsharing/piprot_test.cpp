8822b5c24a89f953a7589f914405747c34c8a955
vivek18394 <vivek18394@gmail.com>
2020-07-16 17:22:53 -0400

    test 1 complete

--- src/secretsharing/piprot_test.cpp
+++ src/secretsharing/piprot_test.cpp
@@ -41,7 +41,7 @@ int main()
     uint64_t mxbitGlobal;
     uint64_t MxGlobal[4];
     uint64_t m0Global, m1Global;
-    uint64_t rxGlobal[4], zGlobal, xGlobal[4];
+    uint64_t rxGlobal[4], zGlobal[4], xGlobal[4];
     uint64_t raGlobal[4], rbGlobal[4],r0Global[4], r1Global[4];//ra, rb, r1, r0 values in Z3
     uint64_t r0l[4],r0m[4],r1l[4],r1m[4],ral[4],ram[4],rbl[4],rbm[4];//LSB and MSB of r0, r1, ra, rb
     uint64_t Zl[4], Zm[4];
@@ -67,9 +67,9 @@ int main()
     poly_eval_global(A,X,B,global_res);    //evaluation of polynomial using Ax+B
     poly_eval_test(out, global_res); //comparing both the outputs to compare the result
     //Test Complete - AX +B polynomial evaluation
-/*
-    //Unit Test - 2 OT evaluation
-    //Receiver Part 1-generate bits X and Rx
+
+    //Unit Test - 2 =========================OT evaluation===========================
+    //Receiver Part 1-generate bits x and rx which are bits{0,1}
     generate_bits_4(xGlobal,generator);
     generate_bits_4(rxGlobal,generator);
     OTZ3_R_Part1Packed(xGlobal, rxGlobal);
@@ -84,28 +84,23 @@ int main()
     //generate rb, rbl and rbm
     generate_msb_lsb_Z3(rbGlobal,rbm,rbl,generator);
 
-    OTZ3_S_Packed(r0m,r0l,r1m,r1l,ram,ral,rbm,rbl);
+    OTZ3_S_Packed(r0m,r0l,r1m,r1l,ram,ral,rbm,rbl);//doesn't need to do anything except calling the function
 
     //Receiver Part 2
+    generate_msb_lsb_Z3(zGlobal,Zm,Zl,generator);//generate zGlobal in Z3 and store their bit representation in Zm and Zl
     OTZ3_R_Part2Packed(Rx,Zm,Zl,Wm,Wl);
-    **cout<<endl<<"Printing Wm and Wl"<<endl;
-    for(int i = 0; i < 1; i++)
+
+    cout<<endl<<"Printing Wm and Wl"<<endl;
+    for(int i = 0; i < 4; i++)
     {
-        for(int word_count = 0; word_count < 64; word_count++)
-        {
-            cout<<endl<<((Wm[i]>>word_count) & 1)<<"\t"<<(Wl[i]>>word_count & 1)<<endl;
-            cout<<endl<<(((Wm[i]>>word_count) & 1) << 1)<<endl;
-            WGlobal[i] = (((Wm[i]>>word_count) & 1) << 1) | (Wl[i]>>word_count & 1);
-        }
-        cout<<WGlobal[i]<<endl;
-    }*/
-
-
-
-
-
-
-
+        //cout<<"x = "<<xGlobal[i]<<endl;
+        //cout<<"W = "<<Wm[i]<<"\t"<<Wl[i]<<endl;
+        cout<<"W = "<<((Wm[i]<<1) | Wl[i])<<endl;
+        //cout<<"r0 = "<<r0Global[i]<<endl;
+        //cout<<"r1 = "<<r1Global[i]<<endl;
+        //cout<<"============================"<<endl;
+        //cout<<WGlobal[i]<<endl;
+    }
 
     return 0;
 }
