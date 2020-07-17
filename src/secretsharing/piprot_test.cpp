e0456c384055f71d484a43f73247cf6d42d29c37
vivek18394 <vivek18394@gmail.com>
2020-07-17 17:21:11 -0400

    2nd unit test

--- src/secretsharing/piprot_test.cpp
+++ src/secretsharing/piprot_test.cpp
@@ -41,11 +41,12 @@ int main()
     uint64_t mxbitGlobal;
     uint64_t MxGlobal[4];
     uint64_t m0Global, m1Global;
-    uint64_t rxGlobal[4], zGlobal[4], xGlobal[4];
+    uint64_t rx[4], z[4], x[4];
     uint64_t raGlobal[4], rbGlobal[4],r0Global[4], r1Global[4];//ra, rb, r1, r0 values in Z3
     uint64_t r0l[4],r0m[4],r1l[4],r1m[4],ral[4],ram[4],rbl[4],rbm[4];//LSB and MSB of r0, r1, ra, rb
     uint64_t Zl[4], Zm[4];
     uint64_t Wl[4], Wm[4], WGlobal[4];
+    uint64_t r0unpck[256],r1unpck[256],raunpck[256],rbunpck[256],Zunpck[256];
 
     unsigned seed = 7;    // std::chrono::system_clock::now().time_since_epoch().count();
     std::mt19937 generator(seed);
@@ -70,48 +71,74 @@ int main()
 
     //Unit Test - 2 =========================OT evaluation===========================
     //Receiver Part 1-generate bits x and rx which are bits{0,1}
-    generate_bits_4(xGlobal,generator);
-    generate_bits_4(rxGlobal,generator);
-    OTZ3_R_Part1Packed(xGlobal, rxGlobal);
+    //generate_bits_4(x,generator);
+    //generate_bits_4(rx,generator);
+    OTZ3_R_Part1Packed(X, Rx);
 
     //Sender
     //generate r0, r0l and r0m
-    generate_msb_lsb_Z3(r0Global,r0m,r0l,generator);
+    generate_rand_Z3_packed_Vec_4(r0m,r0l,r0unpck,generator);
     //generate r1, r1l and r1m
-    generate_msb_lsb_Z3(r1Global,r1m,r1l,generator);
+    generate_rand_Z3_packed_Vec_4(r1m,r1l,r1unpck,generator);
     //generate ra, ral and ram
-    generate_msb_lsb_Z3(raGlobal,ram,ral,generator);
+    generate_rand_Z3_packed_Vec_4(ram,ral,raunpck,generator);
     //generate rb, rbl and rbm
-    generate_msb_lsb_Z3(rbGlobal,rbm,rbl,generator);
+    generate_rand_Z3_packed_Vec_4(rbm,rbl,rbunpck,generator);
 
     OTZ3_S_Packed(r0m,r0l,r1m,r1l,ram,ral,rbm,rbl);//doesn't need to do anything except calling the function
 
     //Receiver Part 2
-    generate_msb_lsb_Z3(zGlobal,Zm,Zl,generator);//generate zGlobal in Z3 and store their bit representation in Zm and Zl
+    generate_rand_Z3_packed_Vec_4(Zm,Zl,Zunpck,generator);
+    //generate_msb_lsb_Z3(z,Zm,Zl,generator);//generate zGlobal in Z3 and store their bit representation in Zm and Zl
     OTZ3_R_Part2Packed(Rx,Zm,Zl,Wm,Wl);
 
     cout<<endl<<"Printing Wm and Wl"<<endl;
-    for(int i = 0; i < 4; i++)
-    {
-        uint64_t WWordm = Wm[i];
-        uint64_t WWordl = Wl[i];
-        uint64_t XWord = X[i];
+    uint64_t X_bit, Wm_bit, Wl_bit, r0m_bit, r0l_bit, r1m_bit, r1l_bit;
+    bool test_flag = 1;//test pass
+    bool mismatch_x0 = 0;//mismatch when x = 0
+    bool mismatch_x1 = 0;//mismatch when x = 1
 
+    for(int i = 0; i < 1; i++)
+    {
+        uint64_t Wm_word = Wm[i];
+        uint64_t Wl_word = Wl[i];
+        uint64_t X_word = X[i];
+        uint64_t r0m_word = r0m[i];
+        uint64_t r0l_word = r0l[i];
+        uint64_t r1m_word = r1m[i];
+        uint64_t r1l_word = r1l[i];
         for (int jBit = 0; jBit < 64; jBit++) {
             //TODO: EXAMINE EACH BIT SEPARATELY OF WWordm, WWordl
             //and see that it matches either r0m, r0l or r1m, r1l
             //cepending on the value of the XWord'i bith
-            /*
-            cout << "x = " << xGlobal[i] << endl;
-            //cout<<"W = "<<Wm[i]<<"\t"<<Wl[i]<<endl;
-            cout << "W = " << ((Wm[i] << 1) | Wl[i]) << endl;
-            cout << "r0 = " << r0Global[i] << endl;
-            cout << "r1 = " << r1Global[i] << endl;
-            cout << "============================" << endl;
-            //cout<<WGlobal[i]<<endl;
-             */
+            X_bit = (X_word >> jBit) & 1;
+            Wm_bit = (Wm_word >> jBit) & 1;
+            Wl_bit = (Wl_word >> jBit) & 1;
+            r0m_bit = (r0m_word >> jBit) & 1;
+            r0l_bit = (r0l_word >> jBit) & 1;
+            r1m_bit = (r1m_word >> jBit) & 1;
+            r1l_bit = (r1l_word >> jBit) & 1;
+            if((X_bit == 0) & ((Wl_bit != r0l_bit) | (Wm_bit != r0m_bit)))//Wl,Wm didn't match with r0l and r0m respectively
+            {
+                mismatch_x0 = 1;//error when x=0
+                break;//breaks out of inner loop
+            }
+            if((X_bit == 1) & ((Wl_bit != r1l_bit) | (Wm_bit != r1m_bit)))//Wl,Wm didn't match with r1l and r1m respectively
+            {
+                mismatch_x1 = 1;//error when x=0
+                break;//breaks out of inner loop
+            }
+        }
+        if((mismatch_x0 == 1) | (mismatch_x1 == 1)){
+            test_flag = 0;
+            break;//breaks out of the outer loop
         }
     }
+    if(test_flag == 0)
+        cout<<"Test fails";
+    else
+        cout<<"Test passed";
+
 
     return 0;
 }
