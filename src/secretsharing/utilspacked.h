b7c8935fa64e6a76dbce9e5d07201e2394659f2f
Tzipora Halevi <thalevi@nyu.edu>
2020-07-21 11:30:27 -0400

    generate test packed word added

---
+++ src/secretsharing/utilspacked.h
@@ -0,0 +1,19 @@
+//Header file weak23prf.h
+
+//#ifndef UTIlS_H
+//#define UTILS_H
+
+//functions for dealing with global variables
+
+
+void generate_rand_packed_vector_4(uint64_t vec[4], std::mt19937 &generator);
+void generate_test_packed_vector_4(uint64_t vec[4], std::mt19937 &generator);
+
+void generate_rand_packed_sqMat_4(uint64_t mat[4][256], std::mt19937 &generator);
+void generate_test_packed_sqMat_4(uint64_t mat[4][256], std::mt19937 &generator);
+
+void generate_rand_Z3_packed_Vec_4(uint64_t PackedVecm[4], uint64_t PackedVecl[4], uint64_t unpackedVec[256], std::mt19937 &generator);
+void generate_test_Z3_packed_Vec_4(uint64_t PackedVecm[4], uint64_t PackedVecl[4], uint64_t unpackedVec[256], std::mt19937 &generator);
+
+
+//#endif
