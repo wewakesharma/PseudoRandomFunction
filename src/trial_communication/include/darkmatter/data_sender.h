e05697cf6388c95070ef3f7f7516e9af043e193a
vivek18394 <vivek18394@gmail.com>
2020-08-18 22:19:02 -0400

    trial communication

---
+++ src/trial_communication/include/darkmatter/data_sender.h
@@ -0,0 +1,9 @@
+//
+// Created by Vivek Sharma on 8/6/20.
+//
+
+#ifndef INC_2PC_DATA_SENDER_H
+#define INC_2PC_DATA_SENDER_H
+
+void pi_snd(std::vector<unsigned int>& snd_vec, int& port);
+#endif //INC_2PC_DATA_SENDER_H
