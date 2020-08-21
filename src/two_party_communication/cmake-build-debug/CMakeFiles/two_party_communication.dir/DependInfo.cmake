28288ef2ae0ff95031b19fcf28faa292fcaeb053
vivek18394 <vivek18394@gmail.com>
2020-08-21 09:05:17 -0400

    two party comm using new send/read function

---
+++ src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/comm/client.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/comm/client.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/comm/recv.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/comm/recv.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/comm/server.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/comm/server.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/comm/snd.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/comm/snd.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/src/OT.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/src/OT.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/src/Toeplitz-by-x.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/src/Toeplitz-by-x.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/src/Toeplitz-by-x_p1.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/src/Toeplitz-by-x_p1.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/src/Toeplitz-by-x_p2.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/src/Toeplitz-by-x_p2.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/src/data_receiver.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/src/data_receiver.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/src/data_sender.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/src/data_sender.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/tests/test_OT.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/tests/test_OT.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/tests/test_Toeplitz-by-x.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/tests/test_Toeplitz-by-x.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/tests/test_packedMod2.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/tests/test_packedMod2.cpp.o"
+  "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/tests/test_packedMod3.cpp" "/Users/vivek/Documents/PseudoRandomFunction/src/two_party_communication/cmake-build-debug/CMakeFiles/two_party_communication.dir/tests/test_packedMod3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../comm"
+  "../include"
+  "../include/darkmatter"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/two_party_communication/comm/client.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "snd.h"
+
+using namespace std;
+//Client side
+int main()
+{
+    std::string data = "hello";
+    pi_snd(data);
+    return 0;    
+}
