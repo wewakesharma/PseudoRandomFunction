d80c12648b63d784acb31b7ead8f36dcb688a445
vivek18394 <vivek18394@gmail.com>
2020-08-22 09:17:24 -0400

    timings added to deoc

---
+++ src/darkmatter_time/cmake-build-debug/CMakeFiles/darkmatter_time.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/darkmatter_time.dir/src/OT.cpp.o"
+  "CMakeFiles/darkmatter_time.dir/src/PRF.cpp.o"
+  "CMakeFiles/darkmatter_time.dir/src/Toeplitz-by-x.cpp.o"
+  "CMakeFiles/darkmatter_time.dir/tests/test_OT.cpp.o"
+  "CMakeFiles/darkmatter_time.dir/tests/test_PRF.cpp.o"
+  "CMakeFiles/darkmatter_time.dir/tests/test_Toeplitz-by-x.cpp.o"
+  "CMakeFiles/darkmatter_time.dir/tests/test_packedMod2.cpp.o"
+  "CMakeFiles/darkmatter_time.dir/tests/test_packedMod3.cpp.o"
+  "darkmatter_time"
+  "darkmatter_time.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/darkmatter_time.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/darkmatter_time/include/darkmatter/mains.hpp
@@ -0,0 +1,16 @@
+#ifndef _EXECUTION_HEADER_H_
+#define _EXECUTION_HEADER_H_
+
+//#define DEBUG
+
+
+// Uncomment one and only one
+//#define TEST_PHASE1
+//#define TEST_PackedMod2
+//#define TEST_PackedMod3
+//#define TEST_PRF
+#define TEST_SC
+
+
+
+#endif // _EXECUTION_HEADER_H_
