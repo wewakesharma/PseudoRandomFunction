3133c9345bc26f6c36f1d145e7e7330ba9837c4b
Tzipora Halevi <thalevi@nyu.edu>
2021-01-18 15:56:26 -0500

    unpack_output fixed

--- src/newprotocol/src/integer_pack.cpp
+++ src/newprotocol/src/integer_pack.cpp
@@ -59,6 +59,7 @@ void unpack_output(PackedZ3<81>& out_Z3, std::vector<uint64_t>& out_pack)//this 
         for(int inner_count = 0; inner_count<6;inner_count++)
         {
             extracted_bit = packed_word>>(inner_count*10);
+            extracted_bit = extracted_bit & 0x3ff;
             out_Z3.set(word_count*6+inner_count,extracted_bit);
 
         }
